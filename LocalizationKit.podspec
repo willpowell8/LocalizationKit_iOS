e709e8c7e30a9c31d349f2f42091b1dfba002f73
Will Powell <willpowell8@gmail.com>
2016-11-08 23:33:15 +0000

    initial commit

--- LocalizationKit.podspec
+++ LocalizationKit.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'LocalizationKit'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of LocalizationKit.'
+  s.summary          = 'iOS Localization made easy and in realtime.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -21,12 +21,12 @@ Pod::Spec.new do |s|
 TODO: Add long description of the pod here.
                        DESC
 
-  s.homepage         = 'https://github.com/<GITHUB_USERNAME>/LocalizationKit'
+  s.homepage         = 'http://github.com/<GITHUB_USERNAME>/LocalizationKit'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
-  s.author           = { 'Will Powell' => 'willpowell8@gmail.com' }
-  s.source           = { :git => 'https://github.com/<GITHUB_USERNAME>/LocalizationKit.git', :tag => s.version.to_s }
-  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
+  s.author           = { 'Will Powell' => '' }
+  s.source           = { :git => 'https://github.com/willpowell8/LocalizationKit.git', :tag => s.version.to_s }
+  s.social_media_url = 'https://twitter.com/willpowelluk'
 
   s.ios.deployment_target = '8.0'
 
@@ -37,6 +37,6 @@ TODO: Add long description of the pod here.
   # }
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+  s.frameworks = 'UIKit'
+  s.dependency 'Socket.IO-Client-Swift', '~>8.1.1'
 end
---
+++ LocalizationKit/Classes/LocalizationKit.swift
@@ -0,0 +1,144 @@
+//
+//  Language.swift
+//  Pods
+//
+//  Created by Will Powell on 03/11/2016.
+//
+//
+
+import Foundation
+import SocketIO
+
+public class LocalizationKit {
+    
+    public static var server:String = "http://10.150.30.48:3000"//"http://178.62.96.76:3000";//"http://192.168.0.5:3000";//"http://178.62.96.76:3000";
+    
+    public static var bundle:Bundle?
+    public static var languageCode:String?
+    public static var socket:SocketIOClient?
+    
+    private static var appKey:String?
+    
+    
+    private static var loadedLanguage:String?
+    private static var loadedLanguageTranslations:[AnyHashable:String]?
+    
+    public static var ALL_CHANGE = Notification.Name(rawValue: "LOCALIZATION_CHANGED")
+    
+    
+    public static func start(appKey:String){
+        self.appKey = appKey
+        initialLanguage();
+        
+        startSocket();
+        
+    }
+    
+    private static func loadLanguage(key:String){
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let urlString = Language.server+"/api/app/\((self.appKey)!)/language/\(key)"
+        let url = URL(string: urlString as String)
+        session.dataTask(with: url!) {
+            (data, response, error) in
+            if (response as? HTTPURLResponse) != nil {
+                let dataString = String(data: data!, encoding: .utf8)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary
+                    loadedLanguageTranslations = json?["data"] as! [AnyHashable:String];
+                    self.joinLanguageRoom()
+                    NotificationCenter.default.post(name: ALL_CHANGE, object: self)
+                    
+                } catch {
+                    print("error serializing JSON: \(error)")
+                }
+                
+            }
+            }.resume()
+    }
+    
+    private static func joinLanguageRoom(){
+        let languageRoom = "\((self.appKey)!)_\((self.languageCode)!)"
+        sendMessage(type: "join", data: ["room":languageRoom])
+    }
+    
+    private static func initialLanguage(){
+        let defs = UserDefaults.standard
+        let languages:NSArray = (defs.object(forKey: "AppleLanguages") as? NSArray)!
+        let current:String  = languages.object(at: 0) as! String
+        let currentParts = current.characters.split{$0 == "-"}.map(String.init)
+        let currentLanguage:String = currentParts[0] as String
+        setLanguage(currentLanguage)
+    }
+    
+    
+    private static func startSocket(){
+        let url = URL(string: server)
+        socket = SocketIOClient(socketURL: url!)
+        socket?.on("connect", callback: {(data,ack) in
+            self.joinLanguageRoom()
+            let appRoom = "\((self.appKey)!)_app"
+            sendMessage(type: "join", data: ["room":appRoom])
+            NotificationCenter.default.post(name: ALL_CHANGE, object: self)
+        })
+        socket?.on("languages", callback: {(data,ack) in
+            let dictionary = data[0] as! [AnyHashable : Any]
+        })
+        socket?.on("highlight", callback: {(data,ack) in
+            let dictionary = data[0] as! [AnyHashable : Any]
+            let meta = dictionary["meta"] as! String
+            let event = "LOC_HIGHLIGHT_\(meta)"
+            NotificationCenter.default.post(name: Notification.Name(rawValue: event), object: self)
+        })
+        socket?.on("text", callback: {(data,ack) in
+            let dictionary = data[0] as! [AnyHashable : Any]
+            let meta = dictionary["meta"] as! String
+            let value = dictionary["value"] as! String
+            self.loadedLanguageTranslations?[meta] = value
+            let event = "LOC_TEXT_\(meta)"
+            NotificationCenter.default.post(name: Notification.Name(rawValue: event), object: self)
+        })
+        socket?.connect()
+    }
+    
+    
+    private static func joinRoom(name:String){
+        self.sendMessage(type: "join", data: ["room":name])
+    }
+    
+    private static func leaveRoom(name:String){
+        self.sendMessage(type: "leave", data: ["room":name])
+    }
+    
+    private static func sendMessage(type:String, data:SocketData...){
+        if socket?.status == SocketIOClientStatus.connected {
+            socket?.emit(type, with: data)
+        }
+    }
+    
+    
+    public static func setLanguage(_ language:String){
+        if languageCode != language {
+            languageCode = language
+            loadLanguage(key: language);
+            //NotificationCenter.default.post(name: Notification.Name(rawValue: "LOCALIZATION_CHANGED"), object: self)
+        }
+    }
+    
+    
+    public static func get(_ key:String, alternate:String) -> String{
+        let m = self.loadedLanguageTranslations
+        if m == nil {
+            return key
+        }
+        
+        guard let localisation = loadedLanguageTranslations?[key] else {
+            if languageCode != nil && socket?.status == SocketIOClientStatus.connected {
+                self.loadedLanguageTranslations?[key] = key
+                self.sendMessage(type: "key", data: ["appuuid":self.appKey!, "key":key, "language":languageCode!])
+            }
+            return key;
+        }
+        return localisation
+    }
+}
