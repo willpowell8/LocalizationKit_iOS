4b1604fd7523d5f656da56525ad1fef1ca513427
Will Powell <willpowell8@gmail.com>
2016-12-24 20:21:48 +0000

    moved to https

--- LocalizationKit/Classes/LocalizationKit.swift
+++ LocalizationKit/Classes/LocalizationKit.swift
@@ -11,7 +11,7 @@ import SocketIO
 
 public class Localization {
     
-    public static var server:String = "http://www.localizationkit.com";
+    public static var server:String = "https://www.localizationkit.com";
     
     public static var bundle:Bundle?
     public static var languageCode:String?
