28e675bd9671d4783a2a1c2a2522f39b045a01d3
Will Powell <willpowell8@gmail.com>
2017-01-02 21:24:11 +0000

    added uibarbuttonitem

---
+++ LocalizationKit/Classes/UIBarButtonItem+Localization.swift
@@ -0,0 +1,18 @@
+//
+//  UIBarButtonItem+Localization.swift
+//  Pods
+//
+//  Created by Will Powell on 02/01/2017.
+//
+//
+
+import Foundation
+
+extension UIBarButtonItem {
+    override public func updateLocalisation() {
+        if( self.title != nil && (self.title?.characters.count)!>0 ){
+            super.updateLocalisation();
+        }
+        
+    }
+}
