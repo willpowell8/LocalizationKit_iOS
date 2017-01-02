425bd01662db4b624d881ef3e4b7f3e24fc0b45f
Will Powell <willpowell8@gmail.com>
2017-01-02 21:53:41 +0000

    update to add Textfield and button

---
+++ LocalizationKit/Classes/UIButton+Localization.swift
@@ -0,0 +1,13 @@
+//
+//  UIButton+Localization.swift
+//  Pods
+//
+//  Created by Will Powell on 02/01/2017.
+//
+//
+
+import Foundation
+
+extension UIButton {
+    
+}
---
+++ LocalizationKit/Classes/UITextField+Localization.swift
@@ -0,0 +1,13 @@
+//
+//  UITextField+Localization.swift
+//  Pods
+//
+//  Created by Will Powell on 02/01/2017.
+//
+//
+
+import Foundation
+
+extension UITextField {
+    
+}
