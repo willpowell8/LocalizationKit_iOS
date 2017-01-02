ea4b5d7ca9bb515b4323803daa0164d90e88ef26
Will Powell <willpowell8@gmail.com>
2017-01-02 20:08:32 +0000

    update of package swift for SPM

---
+++ Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  LocalizationKit
+//
+//  Created by Will Powell on 02/01/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+
+import PackageDescription
+
+let package = Package(
+    name: "LocalizationKit"
+)
