ee80ab9d2c6bf48a4d23798f27493fad3002a3d6
Will Powell <willpowell8@gmail.com>
2017-01-08 15:20:22 +0000

    tidy up of code

--- Example/LocalizationKit/ViewController.swift
+++ Example/LocalizationKit/ViewController.swift
@@ -13,17 +13,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func changeLanguage(_ sender:AnyObject){
-        
-        
         let localizedString = "Select Language".localize
         print("\(localizedString!)");
         
